9c3fce2f20931d9983bd3f790f2704088a859eeb
Cauã Felype <programandocomexcelencia@gmail.com>
2026-05-29 14:33:16 -0300

    refactor: relocate OrderOpsApplication to root package com.orderops and configure maven mainClass

---
+++ backend/src/main/java/com/orderops/OrderOpsApplication.java
@@ -0,0 +1,11 @@
+package com.orderops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderOpsApplication {
+    public static void main(String[] eloquenceArgs) {
+        SpringApplication.run(OrderOpsApplication.class, eloquenceArgs);
+    }
+}
